114af311df1a24e4b27726c5c85319befa69d09c
Enric <qbreis@gmail.com>
2024-03-29 18:47:55 +0100

    feat: adds onclick handler to make the balloon to explode

--- documentation/walkthrough/chapter-02-balloon.md
+++ documentation/walkthrough/chapter-02-balloon.md
@@ -241,6 +241,91 @@ export default function Balloon({ color }) {
 [... rest of the code stays the same...]
 ```
 
+### Balloon blowing when I pop it
+
+I want to do:
+
+- Add state variable `isPopped`
+- Define the callback function to make the balloon to explode when clicked
+- Nest all previous balloon div structure in new div with the `balloon--popping` class name plus the onclick handler.
+
+For this I update `src/components/Balloon/Balloon.jsx`:
+
+```js
+[... rest of the code stays the same...]
+
+export default function Balloon({ color }) {
+
+    // Declare state variable isPopped, with initial value set to false, 
+    // and function setIsPopped to update its value.
+    const [isPopped, setIsPopped] = useState(false); 
+
+    const [isActive, setIsActive] = useState(false);
+    const isMoving = true;
+
+    useEffect(() => {
+        const activeInterval = setInterval(() => {
+            setIsActive((prevIsTrue) => !prevIsTrue);
+        }, 1000);
+        return () => {
+            clearInterval(activeInterval);
+        };
+    }, []);
+
+    // Add function callback to perform specific actions when a balloon is clicked
+    const clickHandler = () => {
+        if (!isPopped) {
+            setIsPopped(true);
+        
+            setTimeout(() => {
+                setIsPopped(false);
+            }, 1000);
+        }
+    };
+
+    // Dynamically constructs a string of CSS class names based on the values of isMoving and isActive variables.
+    const classNames = `
+        balloon 
+        ${isMoving ? 'balloon--moving' : ''} 
+        ${isActive ? 'balloon--active' : ''}
+    `;
+
+[... rest of the code stays the same...]
+
+    // I will add clickHandler to the parent container of the balloon element
+    return (
+        <div className="balloon-cell">
+            <div className="balloon-wrapper">
+                <div 
+                    className={isPopped ? 'balloon--popping' : ''}
+                    onClick={clickHandler}
+                    >
+                    <div 
+                        className={classNames}
+                        style={{ color: color }} 
+                        >
+
+[... rest of the code stays the same...]
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+```
+
+Finally I just add `balloon--popping` class in `src/components/Balloon/Balloon.css`:
+
+```css
+.balloon--popping {
+    transform: scale(2);
+    transform-origin: 50% 20%;
+    opacity: 0;
+    transition: all .2s;
+}
+```
+
 ## Reference links
 
 - [React Pop a Popup Balloon](https://github.com/qbreis/react-pop-a-popup-balloon/) - Link to this GitHub repo.
--- src/components/Balloon/Balloon.jsx
+++ src/components/Balloon/Balloon.jsx
@@ -1,38 +1,34 @@
 import "./Balloon.css";
-
-// import the React Hooks useState and useEffect
 import React, { useState, useEffect } from 'react'; 
 
 export default function Balloon({ color }) {
 
-    // Declare state variable isActive, with initial value set to false, 
-    // and function setIsActive to update its value.
-    const [isActive, setIsActive] = useState(false); 
+    // Declare state variable isPopped, with initial value set to false, 
+    // and function setIsPopped to update its value.
+    const [isPopped, setIsPopped] = useState(false); 
 
-    // Declares isMoving as a constant variable, always true.
-    // Unlike isActive, which is a state variable managed by useState, 
-    // isMoving is a regular constant variable and does not trigger re-renders when its value changes. 
-    // It's just a regular JavaScript variable holding a boolean value.
+    const [isActive, setIsActive] = useState(false);
     const isMoving = true;
 
-    // declares a useEffect hook which sets up an interval to toggle the value of isActive every second, 
-    // and it clears the interval when the component unmounts. 
-    // This effectively creates a blinking effect where the isActive state alternates between true and false every second.
     useEffect(() => {
         const activeInterval = setInterval(() => {
-            // This interval function toggles the value of isActive every 1000 milliseconds (1 second).
-            // It uses the functional update form of setIsActive, 
-            // which receives the previous state (prevIsTrue) and toggles it.
             setIsActive((prevIsTrue) => !prevIsTrue);
         }, 1000);
-    
-        // This is the cleanup function for the effect. 
-        // It's called when the component unmounts or when a new effect is triggered. 
-        // In this case, it clears the interval set up by setInterval to avoid memory leaks
         return () => {
             clearInterval(activeInterval);
         };
     }, []);
+
+    // Add function callback to perform specific actions when a balloon is clicked
+    const clickHandler = () => {
+        if (!isPopped) {
+            setIsPopped(true);
+        
+            setTimeout(() => {
+                setIsPopped(false);
+            }, 1000);
+        }
+    };
 
     // Dynamically constructs a string of CSS class names based on the values of isMoving and isActive variables.
     const classNames = `
@@ -47,49 +43,55 @@ export default function Balloon({ color }) {
     const threadWidth = 10;
     const threadColor = '#ffffff';
     
+    // I will add clickHandler to the parent container of the balloon element
     return (
         <div className="balloon-cell">
             <div className="balloon-wrapper">
                 <div 
-                    className={classNames}
-                    style={{ color: color }} 
+                    className={isPopped ? 'balloon--popping' : ''}
+                    onClick={clickHandler}
                     >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox={`0 0 ${balloonWidth} ${balloonHeight + threadHeight}`}
-                    >
-                        <rect
-                            x={balloonWidth / 2 - threadWidth / 2}
-                            y={balloonHeight}
-                            width={threadWidth}
-                            height={threadHeight}
-                            fill={threadColor}
-                            rx={threadWidth / 2}
-                        />
-                        <polygon
-                            points={`${balloonWidth / 2},${balloonHeight - 3} ${
-                            balloonWidth / 2 + 12
-                            },${balloonHeight + 22} ${balloonWidth / 2 - 12},${
-                            balloonHeight + 22
-                            }`}
-                            fill="currentColor"
-                        />
-                        <ellipse
-                            cx={balloonWidth / 2}
-                            cy={balloonHeight / 2}
-                            rx={balloonWidth / 2}
-                            ry={balloonHeight / 2}
-                            fill="currentColor"
-                        />
-                        <rect
-                            x={balloonWidth / 2 - (threadWidth + 18) / 2}
-                            y={balloonHeight - 1}
-                            width={threadWidth + 18}
-                            height={threadWidth}
-                            fill={threadColor}
-                            rx={threadWidth / 2}
-                        />
-                    </svg>
+                    <div 
+                        className={classNames}
+                        style={{ color: color }} 
+                        >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox={`0 0 ${balloonWidth} ${balloonHeight + threadHeight}`}
+                        >
+                            <rect
+                                x={balloonWidth / 2 - threadWidth / 2}
+                                y={balloonHeight}
+                                width={threadWidth}
+                                height={threadHeight}
+                                fill={threadColor}
+                                rx={threadWidth / 2}
+                            />
+                            <polygon
+                                points={`${balloonWidth / 2},${balloonHeight - 3} ${
+                                balloonWidth / 2 + 12
+                                },${balloonHeight + 22} ${balloonWidth / 2 - 12},${
+                                balloonHeight + 22
+                                }`}
+                                fill="currentColor"
+                            />
+                            <ellipse
+                                cx={balloonWidth / 2}
+                                cy={balloonHeight / 2}
+                                rx={balloonWidth / 2}
+                                ry={balloonHeight / 2}
+                                fill="currentColor"
+                            />
+                            <rect
+                                x={balloonWidth / 2 - (threadWidth + 18) / 2}
+                                y={balloonHeight - 1}
+                                width={threadWidth + 18}
+                                height={threadWidth}
+                                fill={threadColor}
+                                rx={threadWidth / 2}
+                            />
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
